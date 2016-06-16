22bb896a7c2ca518ca06ff58acb9cbe925cb136b
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2016-06-16 07:51:47 -0400

    Implemented parts of the typing.

--- dist/node-jsonapi-datastore.d.ts
+++ dist/node-jsonapi-datastore.d.ts
@@ -1,5 +1,20 @@
-declare namespace jsonapi-datastore {
-
+export class JsonApiDataStore<T> {
+  constructor();
+  destroy(model:T):void;
+  find(type:string, id:string):T;
+  findAll(type:string):Array<T>;
+  reset():void;
+  initModel(type:string, id:string):JsonApiDataStoreModel;
+  
 }
 
-export = jsonapi-datastore;
+interface JsonApiSerializationOptions {
+  attributes?:Array<string>;
+  relationships?:Array<string>;
+}
+
+export class JsonApiDataStoreModel {
+  constructor(type:string, id:string);
+  serialize(options:JsonApiSerializationOptions):any;
+}
+
